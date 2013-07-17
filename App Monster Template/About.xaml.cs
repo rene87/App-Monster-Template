1536f9bfac75e382010c60c4aab22d094d98af7d
Malte Lantin <fake@malte.lantin>
2013-07-17 20:32:17 +0200

    Recommit with gitignore

---
+++ App Monster Template/About.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.ApplicationSettings;
+using Windows.UI.ViewManagement;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace AppMonsters
+{
+    public sealed partial class About : UserControl
+    {
+
+        public About()
+        {
+            this.InitializeComponent();
+        }
+        private void MySettingsBackClicked(object sender, RoutedEventArgs e)
+        {
+            if (this.Parent.GetType() == typeof(Popup))
+            {
+                ((Popup)this.Parent).IsOpen = false;
+            }
+            SettingsPane.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var parent = this.Parent as Popup;
+
+            if (parent != null)
+            {
+                parent.IsOpen = false;
+            }
+
+            if (ApplicationView.Value != ApplicationViewState.Snapped)
+            {
+                SettingsPane.Show();
+            }
+        }
+
+    }
+}
