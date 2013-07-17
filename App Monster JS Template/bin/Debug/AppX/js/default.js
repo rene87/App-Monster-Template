bf37f65f09617cf00a07c7224c32ad8c3aa99e94
Malte Lantin <fake@malte.lantin>
2013-07-17 11:50:13 +0200

    Innitial commit

---
+++ App Monster JS Template/bin/Debug/AppX/js/default.js
@@ -0,0 +1,150 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+    "use strict";
+
+    var myAppMonster;
+
+    WinJS.Binding.optimizeBindingReferences = true;
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                // TODO: This application has been newly launched. Initialize
+                // your application here.
+            } else {
+                // TODO: This application has been reactivated from suspension.
+                // Restore application state here.
+            }
+            args.setPromise(WinJS.UI.processAll().then(function completed() {
+                
+                createAppMonster();
+
+                registerForSharing(); 
+            }));
+        }
+    };
+
+    app.oncheckpoint = function (args) {
+        // TODO: This application is about to be suspended. Save any state
+        // that needs to persist across suspensions here. You might use the
+        // WinJS.Application.sessionState object, which is automatically
+        // saved and restored across suspension. If you need to complete an
+        // asynchronous operation before your application is suspended, call
+        // args.setPromise().
+    };
+
+    app.onloaded = function () {
+        WinJS.Resources.processAll();
+    }
+
+    WinJS.Application.onsettings = function (e) {
+        e.detail.applicationcommands = {
+            "aboutDiv": { title: WinJS.Resources.getString('aboutEntrypoint').value, href: "default.html" }
+        };
+        WinJS.UI.SettingsFlyout.populateSettings(e);
+    };
+
+
+    app.start();
+
+    function createAppMonster() {
+
+        // First we define the image file for your App Monster
+        // This sample image is based on: OCAL, http://www.clker.com/clipart-26433.html, License: Public Domain
+        var imageUri = new Windows.Foundation.Uri("ms-appx:///images/AppMonsterTemplateAvatar.png");
+
+        // Set the values for you own App Monster here. Check wether your App Monster complies with the rules
+
+        // The name is restricted to 30 letter, the description to 300 letters. You should think about localizing the description.
+        var appMonsterName = "Template Monster ";
+        var appMonsterDefaultDescription = "This is the english description for the Template Monster. This is the english description for the Template Monster.";
+
+        // Is your App Monster a paid App? Rare (paid) App Monster have 20 more skillpoints you can use. The maximum allowed prize is an equivalent of € 4.99
+        var paid = false;
+
+        // Set the values for your App Monster here. You can use up to 40 points for a free and up to 60 for a paid App Monster
+        var defense = 8;
+        // This is the damage your monster can take in a fight
+        var hitpoints = 20;
+        // This is the healing power of you monster
+        var heal = 3;
+
+        // Define up to two attacks for your AppMonster;
+        var firstAttack = new AppMonsters.AppMonsterAttack("Firepunch", 3);
+        firstAttack.addLocalizedName("en-US", "Firepunch");
+        firstAttack.addLocalizedName("de-DE", "Feuerhieb");
+
+        var secondAttack = new AppMonsters.AppMonsterAttack("Faststrike", 3);
+        secondAttack.addLocalizedName("en-US", "Faststrike");
+        secondAttack.addLocalizedName("de-DE", "Schnellschlag");
+
+        // Now we need to load the image and create you App Monster
+        Windows.Storage.StorageFile.getFileFromApplicationUriAsync(imageUri).then(function (imageFile) {
+
+            // This where things get exciting. Here we try to bring you new App Monster to life. While doing this we check whether you App Monster complies with the rules. Feels like being Frankenstein, doesn't it?
+            myAppMonster = new AppMonsters.AppMonster(appMonsterName, appMonsterDefaultDescription, firstAttack, secondAttack, defense, hitpoints, heal, paid, imageFile, imageUri);
+
+            // You can add localized descriptions for your App Monster - this is optional. You still have to comply with the 300 letter limit. The translations in the example were created using http://translate.bing.com.
+            myAppMonster.addLocalizedDescription("en-US", "This is the english description for the Template Monster. This is the english description for the Template Monster.");
+            myAppMonster.addLocalizedDescription("de-DE", "Dies ist die deutsche Beschreibung für das Template Monster. Dies ist die deutsche Beschreibung für das Template Monster.");
+
+            // Set you new AppMonster as the Datacontext for this page to display the App Monster Information
+            setDataContext();
+        });
+    }
+
+    // This code makes your monster visible on the main page of your app.
+    function setDataContext() {
+        var template = document.getElementById("monsterTemplate");
+
+        WinJS.Binding.processAll(template, {
+            name: myAppMonster.name,
+            primaryAttack: {
+                localizedName: myAppMonster.primaryAttack.localizedName,
+                damage: myAppMonster.primaryAttack.damage
+            },
+            secondaryAttack: {
+                localizedName: myAppMonster.secondaryAttack.localizedName,
+                damage: myAppMonster.secondaryAttack.damage
+            },
+            defense: myAppMonster.defense,
+            heal: myAppMonster.heal,
+            foodCost: myAppMonster.foodCost,
+            localizedDescription: myAppMonster.localizedDescription,
+            hitPoints: myAppMonster.hitPoints,
+            imageUri: myAppMonster.imageUri.absoluteUri
+        });
+    }
+
+
+    function registerForSharing() {
+        var dataTransferManager = Windows.ApplicationModel.DataTransfer.DataTransferManager.getForCurrentView();
+        dataTransferManager.addEventListener("datarequested", shareAppMonsterHandler);
+    }
+
+    // If you want to open the sharing menu programmatically
+    //function showShareUI() {
+    //    Windows.ApplicationModel.DataTransfer.DataTransferManager.showShareUI();
+    //}
+
+    function shareAppMonsterHandler(e) {
+
+        var request = e.request;
+
+        request.data = myAppMonster.getShareData();
+        
+        //TODO Add remaining content
+        // If you want to modify the default title, description or text for sharing from this page you can do it here
+        //request.data.properties.title = "This is " + myAppMonster.name + " - the lastest addition to my App Monster team";
+        //request.data.properties.description = "Share " + myAppMonster.name + " with the App Monsters Hub App";
+        //request.data.setText("I just downloaded a new App Monster from the Windows Store: " + myAppMonster.name + "! Check it out yourself at http://apps.microsoft.com/windows/app/" + myAppMonster.appId);        
+        
+    }
+})();
+
+
+
---
+++ App Monster Template/App.xaml.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.Resources;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ApplicationSettings;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=234227
+
+namespace AppMonsters
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : Application
+    {
+        // Used to include the settings and the about flyout
+        private const int settingsFlyoutWidth = 346;
+        private Rect windowBounds;
+        private Popup settingsPopup;
+
+        // Needed to load localized strings from the resource file in code
+        private ResourceLoader resourceLoader;
+
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+            resourceLoader = new ResourceLoader();
+        }
+
+        #region Settings
+
+        private void InitializeSettings()
+        {
+            windowBounds = Window.Current.Bounds;
+            Window.Current.SizeChanged += Current_SizeChanged;
+            SettingsPane.GetForCurrentView().CommandsRequested += App_CommandsRequested;
+        }
+
+        void Current_SizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
+        {
+            windowBounds = Window.Current.Bounds;
+        }
+
+        void App_CommandsRequested(SettingsPane sender, SettingsPaneCommandsRequestedEventArgs args)
+        {
+            args.Request.ApplicationCommands.Add(new SettingsCommand("about", resourceLoader.GetString("AboutEntrypoint"), (command) =>
+            {
+                ShowSettingsFlyout(new About());
+            }));
+        }
+
+        private void ShowSettingsFlyout(UserControl content)
+        {
+            settingsPopup = new Popup();
+
+            settingsPopup.Closed += popup_Closed;
+            Window.Current.Activated += Current_Activated;
+
+            settingsPopup.IsLightDismissEnabled = true;
+            settingsPopup.Width = settingsFlyoutWidth;
+            settingsPopup.Height = windowBounds.Height;
+
+            settingsPopup.ChildTransitions = new TransitionCollection();
+            settingsPopup.ChildTransitions.Add(new PaneThemeTransition()
+            {
+                Edge = (SettingsPane.Edge == SettingsEdgeLocation.Right) ? EdgeTransitionLocation.Right : EdgeTransitionLocation.Left
+            });
+
+            content.Width = settingsFlyoutWidth;
+            content.Height = windowBounds.Height;
+
+            settingsPopup.Child = content;
+
+            settingsPopup.SetValue(Canvas.LeftProperty, SettingsPane.Edge == SettingsEdgeLocation.Right ? windowBounds.Width - settingsFlyoutWidth : 0);
+            settingsPopup.SetValue(Canvas.TopProperty, 0);
+
+            settingsPopup.IsOpen = true;
+        }
+
+        void Current_Activated(object sender, Windows.UI.Core.WindowActivatedEventArgs e)
+        {
+            if (e.WindowActivationState == Windows.UI.Core.CoreWindowActivationState.Deactivated)
+            {
+                settingsPopup.IsOpen = false;
+            }
+        }
+
+        void popup_Closed(object sender, object e)
+        {
+            Window.Current.Activated -= Current_Activated;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used when the application is launched to open a specific file, to display
+        /// search results, and so forth.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+                if (args.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Load state from previously suspended application
+                }
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+
+                // Activate the settings
+                InitializeSettings();
+            }
+
+            if (rootFrame.Content == null)
+            {
+                // When the navigation stack isn't restored navigate to the first page,
+                // configuring the new page by passing required information as a navigation
+                // parameter
+                if (!rootFrame.Navigate(typeof(MainPage), args.Arguments))
+                {
+                    throw new Exception("Failed to create initial page");
+                }
+            }
+            // Ensure the current window is active
+            Window.Current.Activate();
+        }
+
+        /// <summary>
+        /// Invoked when application execution is being suspended.  Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
+        /// </summary>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity
+            deferral.Complete();
+        }
+    }
+}
